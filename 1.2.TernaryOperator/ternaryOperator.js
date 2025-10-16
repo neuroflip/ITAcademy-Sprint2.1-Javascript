aaf01e2681b62aab10e1573fef43aa81fc6de48f
neuroflip <jaanguita@gmail.com>
2025-10-16 23:16:09 +0200

    Simplified trobarMaxim and changed the screenshot with all the tests

--- 1.2.TernaryOperator/ternaryOperator.js
+++ 1.2.TernaryOperator/ternaryOperator.js
@@ -2,9 +2,8 @@ const potConduir = (edat) => edat >= 18 ? 'Pots conduir' : 'No pots conduir';
 const comparador = (num1, num2) => num1 > num2 ? 'num1 es mes gran' : 'num2 es mes gran';
 const positiuNegatiuOZero = (num) => num > 0 ? 'positiu' : num < 0 ? 'negatiu' : 'zero';
 const trobarMaxim = (a, b, c) => {
-  return a > b ? (a > c ? a : c) : 
-    (b > c ?  (b > a ? b : a) : 
-    (c > a ? (c > b ? c : b) : a));
+  return (a > b) && (a > c) ? a : 
+    (b > c) && (b > a) ? b : c;
 }
 const parOImpar = (nums) => {
   for (let i=0; i< nums.length; i++) {
